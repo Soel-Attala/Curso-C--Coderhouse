37462fb0ba52097412404251bbd9f7a19376708e
Soel Attala <soelattala7@gmail.com>
2023-12-26 11:19:35 -0300

    adding excercise 2

--- Lesson2-ConsoleApp/Program.cs
+++ Lesson2-ConsoleApp/Program.cs
@@ -44,6 +44,7 @@ Console.ReadKey();
  * 
  */
 
+/*
 string category;
 
 Console.WriteLine("Please insert a category A, B or C");
@@ -66,4 +67,60 @@ switch (category)
 
 Console.ReadKey();
 
+*/
+
+/*La secretaria de una universidad nos pide un sistema para poder cargar los datos de sus alumnos, 
+ * necesitan poder cargar la comisión que puede ser A si son de turno mañana, B si son de turno tarde, 
+ * C si son de turno noche y en caso de no tener comisión indica que no tienen turno fijado.
+
+También cada docente califica a sus alumnos con una nota entre 0 y 10, 
+en caso de ser menor a 4 el alumno Recursa la Materia, si está entre 4 y 7 es Aprobó Cursada 
+y si es mayor que 7 Promociona la Materia.
+*/
+
+string course;
+
+Console.WriteLine("Please insert a course A,B or C");
+Console.WriteLine("A. Morning course");
+Console.WriteLine("B. Afternoon course");
+Console.WriteLine("C. Night course");
+Console.WriteLine("D. Not enrolled in any courses");
+
+course = Console.ReadLine();
+
+switch (course)
+{
+    case "A":
+        Console.WriteLine("The student belongs to the morning shift course");
+        break;
+    case "B":
+        Console.WriteLine("The student belongs to the afternoon shift course");
+        break;
+    case "C":
+        Console.WriteLine("The student belongs to the night shift course");
+        break;
+    default:
+        Console.WriteLine("This student is not enrolled in any courses");
+        break;
+}
+
+
+int califications;
+Console.WriteLine("Please instert the student calification: ");
+califications = Convert.ToInt32(Console.ReadLine());
+
+if (califications < 4)
+{
+    Console.WriteLine("The student must retake the course");
+}
+else if (califications >= 4 || califications <= 7)
+{
+    Console.WriteLine("The student has passed the course");
+}
+else
+{
+    Console.WriteLine("The student promoted the course");
+}
+
+Console.ReadKey();
 

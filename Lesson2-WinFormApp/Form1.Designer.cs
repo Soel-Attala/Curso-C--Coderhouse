8d53103780308404950822f5f04ad31d3190bbc8
Soel Attala <soelattala7@gmail.com>
2024-01-04 09:08:29 -0300

    lesson 4

---
+++ Lesson2-WinFormApp/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Lesson2_WinFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            ProfileBox = new TextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Location = new Point(367, 230);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Profile:";
+            label1.UseMnemonic = false;
+            label1.Click += label1_Click;
+            // 
+            // ProfileBox
+            // 
+            ProfileBox.Location = new Point(432, 227);
+            ProfileBox.Name = "ProfileBox";
+            ProfileBox.Size = new Size(100, 23);
+            ProfileBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Location = new Point(367, 259);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(432, 256);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.Font = new Font("Trebuchet MS", 11.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button1.Location = new Point(443, 296);
+            button1.Name = "button1";
+            button1.Size = new Size(78, 30);
+            button1.TabIndex = 4;
+            button1.Text = "LogIn";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveCaption;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(1040, 518);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(ProfileBox);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            DoubleBuffered = true;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox ProfileBox;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ Lesson3-ConsoleApp/Program.cs
@@ -0,0 +1,186 @@
+﻿/*
+int num1 = 0;
+int num2 = 0;
+
+greetings();
+greetings();
+void suma()
+{
+    int resultado = num1 + num2;
+    Console.WriteLine($"la suma es: {resultado} ");
+}
+
+Console.WriteLine("Ingrese un numero: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+suma();
+
+
+Console.ReadKey();
+
+
+
+void greetings()
+{
+    Console.WriteLine("Hello im a function");
+}
+
+*/
+
+
+/*
+int passwordCreate = GetValidPassword();
+int passwordVerification = GetPasswordVerification(passwordCreate);
+char profile = GetProfile(passwordCreate, passwordVerification);
+
+Console.ReadKey();
+
+
+int GetValidPassword()
+{
+    int passwordCreate;
+
+    while (true)
+    {
+        Console.WriteLine("Please create your password (numeric, at least 8 digits): ");
+        passwordCreate = Convert.ToInt32(Console.ReadLine());
+
+        if (passwordCreate.ToString().Length >= 8)
+        {
+            Console.WriteLine("Password saved");
+            return passwordCreate;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect password, please select a numeric password with 8 or more digits");
+        }
+    }
+}
+
+int GetPasswordVerification(int passwordCreate)
+{
+    int passwordVerification;
+
+    while (true)
+    {
+        Console.WriteLine("Please enter your password to access the system: ");
+        passwordVerification = Convert.ToInt32(Console.ReadLine());
+
+        if (passwordCreate == passwordVerification)
+        {
+            Console.WriteLine("Password verified");
+            return passwordVerification;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect password, please try again");
+        }
+    }
+}
+
+char GetProfile(int passwordCreate, int passwordVerification)
+{
+    char profile;
+
+    while (true)
+    {
+        Console.WriteLine("Please select your profile (A: Administrator, U: User, X: New Profile): ");
+        profile = Convert.ToChar(Console.ReadLine());
+
+        switch (profile)
+        {
+            case 'A':
+                Console.WriteLine("Welcome Administrator");
+                return profile;
+            case 'U':
+                Console.WriteLine("Welcome User");
+                return profile;
+            case 'X':
+                Console.WriteLine("Welcome, undefined profile, please create your profile");
+                return profile;
+            default:
+                Console.WriteLine("Invalid profile");
+                break;
+        }
+    }
+}
+*/
+
+
+// ACTIVIDAD COLABORATIVA
+
+// una tienda desea realizar una venta y desea hacerlo a traves de una aplicacion.
+//tenemos un listado de materiales 
+//el cliente debe seleccionar el producto que desea comprar a traves del codigo.
+
+
+
+
+using System.Xml.Schema;
+
+string products = GetProducts();
+
+string GetProducts()
+{
+    int price;
+    int amount;
+    int total;
+
+    Console.WriteLine($"LISTA DE PRODUCTOS\n DETERGENTE \t\t PRECIO:{price = 300}\n JABON EN POLVO\t\t PRECIO:{price = 200}\n DETERGENTE\t\t PRECIO:{price = 250} ");
+
+    string code;
+
+    while (true)
+    {
+        Console.WriteLine("Porfavor seleccione el producto a comprar o confirme su compra:\n (DES: Desodorante, JP: Jabon en polvo, DET: Detergente, FIN: para confirmar su compra): ");
+        code = Console.ReadLine();
+
+        switch (code)
+        {
+            case "DES":
+                price = 300;
+                Console.WriteLine("Usted selecciono Desodorante.");
+                Console.WriteLine($"El precio Desodorante es: {price}");
+                Console.WriteLine("Por favor indique la cantidad que desea comprar");
+                amount = Convert.ToInt32(Console.ReadLine());
+                total = price * amount;
+                Console.WriteLine($"El total a pagar es: {total}, Desea confirmar su compra?\n Para confirmar su compra introduzca el codigo FIN");
+
+                break;
+            case "JP":
+                price = 200;
+                Console.WriteLine("Usted selecciono Jabon en polvo.");
+                Console.WriteLine($"El precio del Jabon en polvo es: {price}");
+                Console.WriteLine("Por favor indique la cantidad que desea comprar");
+                amount = Convert.ToInt32(Console.ReadLine());
+                total = price * amount;
+                Console.WriteLine($"El total a pagar es: {total}, Desea confirmar su compra?\n Para confirmar su compra introduzca el codigo FIN");
+
+                break;
+            case "DET":
+                price = 250;
+                Console.WriteLine("Usted selecciono Detergente.");
+                Console.WriteLine($"El precio del Detergente es: {price}");
+                Console.WriteLine("Por favor indique la cantidad que desea comprar");
+                amount = Convert.ToInt32(Console.ReadLine());
+                total = price * amount;
+                Console.WriteLine($"El total a pagar es: {total}, Desea confirmar su compra?\n Para confirmar su compra introduzca el codigo FIN");
+
+                break;
+
+            case "FIN":
+                Console.WriteLine("Por favor vuelva a ingresar la cantidad deseada");
+                amount = Convert.ToInt32(Console.ReadLine());
+                total = price * amount;
+                Console.WriteLine($"Compra confirmada el total a pagar es: {total}");
+                return code;
+
+            default:
+                Console.WriteLine("Invalid product");
+                break;
+        }
+    }
+}

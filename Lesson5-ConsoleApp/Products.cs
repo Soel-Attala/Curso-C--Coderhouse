0e0121c2a42d2f299ad1846d719aac81b4997eda
Soel Attala <soelattala7@gmail.com>
2024-01-08 21:26:18 -0300

    creamos clases constructores, instancias y destructores

---
+++ Lesson5-ConsoleApp/Products.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_ConsoleApp
+{
+    internal class Products
+    {
+        private int _code;
+        private string _name;
+        private string _description;
+        private double _buyPrice;
+        private double _sellerPrice;
+        private string _category;
+
+        public Products()
+        {
+            _code = 0;
+            _description = string.Empty;
+            _name = string.Empty;
+            _buyPrice = 0;
+            _sellerPrice = 0;
+            _category = string.Empty;
+
+        }
+
+        public Products(int code, string description, string name, double buyPrice, double sellerPrice, string category)
+        {
+            this._code = code;
+            this._description = description;
+            this._name = name;
+            this._buyPrice = buyPrice;
+            this._sellerPrice = sellerPrice;
+            this._category = category;
+
+        }
+
+    }
+}
---
+++ Lesson5-ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+using Lesson5_ConsoleApp;
+
+Products products = new Products();
+Products products2 = new Products(1, "Pepsi", "bottle of cola", 5000, 10000, "drinks");

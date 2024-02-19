0a46ad3bcdfab994457e96935fb75fa138d0bcd1
Soel Attala <soelattala7@gmail.com>
2024-02-19 10:10:08 -0300

    database creation and update migrations

---
+++ Lesson-11-EntityFramework/database/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Lesson_11_EntityFramework.models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11_EntityFramework.database
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=SOEL-PC\\SQLEXPRESS;Database=Coder290124; Trusted_Connection=True;");
+        }
+    }
+}

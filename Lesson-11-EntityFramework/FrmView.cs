34649fda06ac3001e4aa6e7983f422ae7eb92f8c
Soel Attala <soelattala7@gmail.com>
2024-02-19 09:44:35 -0300

    adding product model to lesson 11

---
+++ Lesson-11-EntityFramework/FrmView.cs
@@ -0,0 +1,15 @@
+namespace Lesson_11_EntityFramework
+{
+    public partial class FrmView : Form
+    {
+        public FrmView()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmView_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lesson-11-EntityFramework/models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11_EntityFramework.models
+{
+    public class Product
+    {
+
+        public int Stock { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int ProductId { get; set; }
+        public double Price { get; set; }
+
+    }
+}
